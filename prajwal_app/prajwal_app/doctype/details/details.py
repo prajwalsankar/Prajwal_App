4f49d0c0ebfe79cc687fd76235e71b8c460bb186
prajwalsankar <prajwalsankar@gmail.com>
2023-08-29 12:07:06 +0530

    Second commit

--- prajwal_app/prajwal_app/doctype/details/details.py
+++ prajwal_app/prajwal_app/doctype/details/details.py
@@ -8,6 +8,6 @@ from frappe.model.document import Document
 class Details(Document):
 	def calculate_age(self):
 		if self.dob:
-        	today = frappe.utils.today()
-        	age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
-        	self.age = age
+			today = frappe.utils.today()
+			age = today.year - self.dob.year - ((today.month, today.day) < (self.dob.month, self.dob.day))
+			self.age = age
